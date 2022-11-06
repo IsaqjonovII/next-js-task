0eef0f57d68c07b7b83cedeb78251fae39f77cec
ilhomjon003 <ilhomjonisoqjonov2@gmail.com>
2022-11-06 23:45:30 +0500

    ready

--- pages/shopnow/index.jsx
+++ pages/shopnow/index.jsx
@@ -13,12 +13,12 @@ const ShopNow = () => {
       </Head>
       <p><span className={c.breadcrumb}>Products</span> / Shop now</p>
       <h1 className={c.checkout__title} >Checkout</h1>
-      <div className={c.input__container}>
+      <form className={c.input__container}>
         <input type="text" className={c.input} placeholder='Search products' />
         <button className={c.input__search__btn}>
           <SearchIcon />
         </button>
-      </div>
+      </form>
       <div className={c.products__container}>
         {
           PRODUCTS__DATA.map((product) => (
@@ -27,7 +27,14 @@ const ShopNow = () => {
         }
       </div>
       <div className={c.pagination}>
-        &#x2190; &#x2192;
+        <div className={c.chevron__icon}>&#x2190; </div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>...</li>
+        </ul>
+        <div className={c.chevron__icon}>&#x2192;</div>
       </div>
     </div>
   )
